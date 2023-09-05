1cc656d5de023999c2c3361f041168fc686fb00e
Andrew Einspanier <aeinspanier23@gmail.com>
2023-09-05 14:15:28 -0500

    adding adapter and iterator packages

--- oopmodule/main.go
+++ oopmodule/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 import "oopmodule/singleton"
 import "oopmodule/factory"
+import . "oopmodule/adapter"
+import . "oopmodule/iterator"
 
 func triggerSingleton() {
 	for i := 0; i < 5; i++ {
@@ -17,10 +19,53 @@ func runFactory() {
 	fmt.Println("Checking Toyota: Make=" + toyota.GetMake() + " Model=" + toyota.GetModel())
 }
 
+func runAdapter() {
+	client := &Client{}
+    mac := &Mac{}
+
+    client.InsertLightningConnectorIntoComputer(mac)
+
+    windowsMachine := &Windows{}
+    windowsMachineAdapter := &WindowsAdapter{
+        WindowMachine: windowsMachine,
+    }
+
+    client.InsertLightningConnectorIntoComputer(windowsMachineAdapter)
+}
+
+func runIterator() {
+	user1 := &User{
+        Name: "a",
+        Age:  30,
+    }
+    user2 := &User{
+        Name: "b",
+        Age:  20,
+    }
+
+    userCollection := &UserCollection{
+        Users: []*User{user1, user2},
+    }
+
+	iterator := userCollection.CreateIterator()
+
+	for iterator.HasNext() {
+		user := iterator.GetNext()
+		fmt.Printf("User is %+v\n", user)
+
+	}
+}
+
 func main() {
 	fmt.Println("Singleton: ")
 	triggerSingleton()
 
 	fmt.Println("Factory: ")
 	runFactory()
+
+	fmt.Println("Adapter: ")
+	runAdapter()
+
+	fmt.Println("Iterator: ")
+	runIterator()
 }
